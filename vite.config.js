428c63accfba57931588545679ac5d55d6b58d47
creep <m15856961172@163.com>
2024-09-11 10:49:24 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,11 @@ import uni from '@dcloudio/vite-plugin-uni'
 // import basicSsl from '@vitejs/plugin-basic-ssl'
 import { defineConfig } from 'vite'
 import { netConfig } from './src/config/net.config'
-// https://vitejs.dev/config/
-const define = {}
-if (!["mp-weixin", "h5", "web"].includes(process.env.UNI_PLATFORM)) {
-  define['global'] = null
-  define['wx'] = 'uni'
-}
+
 export default defineConfig({
   base: '/' ?? '/cmms-mobile',
   plugins: [
-    uni(),
-    define
+    uni()
     // basicSsl()
   ],
   server: {
