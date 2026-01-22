8a5eb4c72aeaac3c28bf61590b95b5d286a6e39d
fang.ss <17775436676@163.com>
2026-01-22 15:34:54 +0800

    feat: 样式更新

--- src/pages/dialogue/components/TopTip.vue
+++ src/pages/dialogue/components/TopTip.vue
@@ -3,12 +3,24 @@
 </script>
 <template>
     <view class=" relative w-full mx-auto">
-        <image class="absolute -top-12px left-22px z-2 w-52px" src="@/static/images/top-tip-icon2.png" mode="widthFix" />
-        <image class="w-full " src="@/static/images/top-tip.png" mode="widthFix" />
-        <view class=" absolute w-full h-full top-0 z-10 p-16px line-height-[1.6]">
-            <view class="text-16px font-600 pl-80px ">您好，我是您的智能AI助手晓言</view>
-            <view class="text-12px font-500 pl-80px ">很高兴与你相遇，工作上碰到难题？</view>
+        <view class="gradient-bg relative rounded-12px h-75px pt-16px">
+            <image class=" absolute left-16px -top-16px w-60px h-60px" src="@/static/images/top-tip-icon2.png"
+                mode="widthFix" />
+            <view class="text-16px font-600 ml-90px">Hi～我是您的智能AI助手晓言</view>
+        </view>
+        <view class="gradient-bg2 rounded-12px border-(0.6px solid #fff) p-15px -mt-24px relative z-10">
+            <view class="text-12px font-500 mb-5px ">很高兴与你相遇，工作上碰到难题？</view>
             <view class="text-12px font-500 ">可以告诉我，我会凭借强大的共能和海量知识，精准分析贴心解答，为你提供清晰思路与实用建议，开启我们愉快的交流互助之旅。</view>
         </view>
     </view>
 </template>
+
+<style scoped>
+.gradient-bg {
+    background: linear-gradient(123.28deg, rgba(197, 255, 247, 1) 0%, rgba(255, 239, 253, 1) 23.68%, rgba(211, 207, 255, 1) 48.31%, rgba(207, 244, 255, 1) 71.91%, rgba(199, 202, 255, 1) 100%);
+}
+
+.gradient-bg2 {
+    background: linear-gradient(134.33deg, #F6F9FF 0%, #E0F4FF 41.01%, #FBFCFF 76.07%, #E9FEFF 100%);
+}
+</style>
